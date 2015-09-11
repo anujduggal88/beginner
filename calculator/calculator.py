27928e4a526f9aa6d663941a3b06e0b5637b0e07
Anirudh Chintha <anirudh.chintha@intel.com>
2015-09-11 16:12:29 +0530

    Add take_numbers function.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,7 +5,7 @@
 #                           DESCRIPTION     :       Calculator Program
 #                           DATE CREATED    :       11 SEPT, 2015
 #                           DATE MODIFIED   :       11 SEPT, 2015
-#                           VERSION         :       1.0
+#                           VERSION         :       1.1
 #
 
 #
@@ -28,5 +28,8 @@ def main():
     choice = display_choices()
     if choice is -1:
         print "PROGRAM NOT COMPLETED"
-
+def take_numbers():
+    input1 = int(raw_input("Enter your 1st number"))
+    input2 = int(raw_input("Enter your 2nd number"))
+    return input1,input2
 main()
